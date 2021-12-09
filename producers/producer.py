9b39f5b21118bf335c0f4033f05e4991a699c9cf
KeshaunW <keshaun.williams@smoothstack.com>
2021-12-09 12:50:46 -0500

    BA-34 Generated the query to be used to insert the dummy data into the database.

--- producers/producer.py
+++ producers/producer.py
@@ -1,7 +1,17 @@
 import openpyxl
 import pandas as pd
+import mysql.connector
 
 from numpy.random import randn
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='aline',
+    password='Aline2022!',
+    database='alinedb'
+)
+
+cursor = db.cursor()
 
 producers = ['user', 'applicant', 'bank', 'branch', 'transaction']
 
@@ -10,13 +20,24 @@ for sheet in range(0, len(producers)):
     print(producers[sheet])
     print(records.columns)
     for row in range(0, len(records)):
+        columnsStr = ''
+        valuesStr = ''
         for column in range(0, len(records.columns)):
-            print(str(records.columns[column]) + ' = \'' + str(records.iloc[row][column]) + '\'', end='')
-            if column + 1 < len(records.columns):
-                print(',')
-        print()
+            columnsStr = columnsStr + str(records.columns[column])
 
+            if str(records.columns[column]) == 'enabled' or str(records.columns[column]) == 'member_id':
+                valuesStr = valuesStr + '' + str(records.iloc[row][column]) + ''
+            else:
+                valuesStr = valuesStr + '\'' + str(records.iloc[row][column]) + '\''
+
+            if column + 1 < len(records.columns):
+                # print(',')
+                columnsStr = columnsStr + ', '
+                valuesStr = valuesStr + ', '
+        query = 'INSERT INTO ' + producers[sheet] + ' (' + columnsStr + ') VALUES (' + valuesStr + ')'
+        print('Query #' + str(row+1) + ': ' + query)
+        # cursor.execute(query)
+        # db.commit()
+        # print(cursor.rowcount, "record inserted.")
     print()
 
-records = pd.read_excel('dummy_data.xlsx', sheet_name=0)
-print(records.iloc[0][1])
